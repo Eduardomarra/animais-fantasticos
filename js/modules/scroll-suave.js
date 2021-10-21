b0be2c7ae2ab625eacdeb7d8428f2e96d4c126af
Eduardo Marra <eduardomarra@gmail.com>
2021-10-20 21:12:19 -0300

    Scroll-suave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,18 +1,32 @@
-export default function initScrollSuave() {
-    const internalLinks = document.querySelectorAll(".js-menu a[href^='#']");
-  
-    function scrollToSection(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
-
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-      });
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.internalLinks = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
     }
-  
-    internalLinks.forEach((link) => {
-      link.addEventListener("click", scrollToSection);
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.internalLinks.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
     });
   }
+
+  init() {
+    if (this.internalLinks.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 import initAminateScroll from "./modules/animate-scroll.js";
-import initTabNav from  "./modules/init-tab.js";
-import initAccordion from "./modules/init-accordion.js"
-import initScrollSuave from "./modules/scroll-suave.js"
-import initModal from "./modules/modal.js"
-import initTooltip from "./modules/tooltip.js"
-import initDropdownMenu from "./modules/dropdown-menu.js"
-import initMenuMobile from "./modules/menu-mobile.js"
-import initFetchAnimais from "./modules/fetch-animais.js"
+import initTabNav from "./modules/init-tab.js";
+import initAccordion from "./modules/init-accordion.js";
+import ScrollSuave from "./modules/scroll-suave.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
 initAminateScroll();
 initTabNav();
 initAccordion();
-initScrollSuave();
 initModal();
 initTooltip();
 initDropdownMenu();
