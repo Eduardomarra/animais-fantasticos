c350fc1c718680a53710d9f74b542f82211c5209
Eduardo Marra <eduardomarra@gmail.com>
2021-12-15 22:18:28 -0300

    menuMobile refatorado

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,23 +1,37 @@
 import outsideClick from "./outside-click.js";
 
-export default function initMenuMobile() {
-  const menuButton = document.querySelector("[data-menu='button'");
-  const menuList = document.querySelector("[data-menu='list'");
-  const eventos = ["click", "touchstart"];
+export default class MenuMobile {
+  constructor(menuButton, menuList, events){
+    this.menuButton = document.querySelector(menuButton);
+    this.menuList = document.querySelector(menuList);
+    this.activeClass = 'active';
 
-  function openMenu() {
-    menuButton.classList.add("active");
-    menuList.classList.add("active");
+    // Define touchstart e click como argumento padrao
+    // de events caso o usuário não defina
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
 
-    outsideClick(menuList, eventos, () => {
-      menuButton.classList.remove("active");
-      menuList.classList.remove("active");
-    });
+    this.openMenu = this.openMenu.bind(this);
   }
 
-  if (menuButton) {
-    eventos.forEach((evento) => {
-      menuButton.addEventListener(evento, openMenu);
+    openMenu() {
+      this.menuList.classList.add(this.activeClass);
+      this.menuButton.classList.add(this.activeClass);
+
+      outsideClick(this.menuList, this.events, () => {
+        this.menuList.classList.remove(this.activeClass);
+        this.menuButton.classList.remove(this.activeClass);
+      });
+  }
+
+  addMenuMobileEvents(){
+    this.events.forEach((evento) => {
+      this.menuButton.addEventListener(evento, this.openMenu);
     });
+  }
+  
+  init(){
+    if (this.menuButton && this.menuList) this.addMenuMobileEvents();
+    return this;
   }
 }
